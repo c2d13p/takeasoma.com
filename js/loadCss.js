dac4ae9a5cad60960e50f603be172d280e404360
c2d13p <c2d13p@gmail.com>
2024-07-31 11:38:15 +0200

    add logic for different color schemes based on month

---
+++ js/loadCss.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const monthClasses = [
+        'january',
+        'february',
+        'march',
+        'april',
+        'may',
+        'june',
+        'july',
+        'august',
+        'september',
+        'october',
+        'november',
+        'december'
+    ];
+
+    //const month = new Date().getMonth();
+    const month = 1;
+    const monthClass = monthClasses[month];
+
+    document.body.classList.add(monthClass);
+    
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach(box => {
+        box.classList.add(monthClass);
+    });
+
+    const footer = document.querySelector('.footer');
+    footer.classList.add(monthClass);
+});
