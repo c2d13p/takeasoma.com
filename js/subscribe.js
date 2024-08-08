a5f119b53cefb242278e40d6d0264bfbe9e58c12
c2d13p <c2d13p@gmail.com>
2024-08-08 11:52:31 +0200

    add google form to blog subscription

--- js/subscribe.js
+++ js/subscribe.js
@@ -1,3 +1,5 @@
+document.getElementById('subscribe-button').addEventListener('click', subscribe);
+
 function subscribe() {
     var type = document.getElementById('subscription-type').value;
     var input = document.getElementById('subscription-input').value;
@@ -8,8 +10,11 @@ function subscribe() {
         return;
     }
 
-    // Simulate sending email (replace with actual email sending code)
     console.log(`New subscription by ${type} @ ${input}`);
+
+    // Fill the hidden form input and submit it
+    document.getElementById('hidden-entry').value = input;
+    document.getElementById('hidden-form').submit();
 
     button.textContent = "subscribed!";
     button.disabled = true;
