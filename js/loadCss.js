4f5585752561bd35708289f9eb8fde14c20f1e89
c2d13p <c2d13p@gmail.com>
2024-09-03 09:27:47 +0200

    improve font loading

--- js/loadCss.js
+++ js/loadCss.js
@@ -14,6 +14,21 @@ document.addEventListener("DOMContentLoaded", function() {
         'december'
     ];
 
+    const monthFonts = {
+        'january': "https://fonts.googleapis.com/css?family=Shadows+Into+Light",
+        'february': "https://fonts.googleapis.com/css?family=Tomorrow",
+        'march': "https://fonts.googleapis.com/css?family=Caveat",
+        'april': "https://fonts.googleapis.com/css?family=Kaushan+Script",
+        'may': "https://fonts.googleapis.com/css?family=Oleo+Script",
+        'june': "https://fonts.googleapis.com/css?family=Playwrite+AT",
+        'july': "https://fonts.googleapis.com/css?family=Coiny",
+        'august': "https://fonts.googleapis.com/css?family=Notable",
+        'september': "https://fonts.googleapis.com/css?family=MuseoModerno",
+        'october': "https://fonts.googleapis.com/css?family=Autour+One",
+        'november': "https://fonts.googleapis.com/css?family=Matemasie",
+        'december': "https://fonts.googleapis.com/css?family=Rubik+Bubbles"
+    };
+
     const month = new Date().getMonth();
     const monthClass = monthClasses[month];
 
@@ -26,4 +41,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const footer = document.querySelector('.footer');
     footer.classList.add(monthClass);
+    
+    const fontLink = document.createElement('link');
+    fontLink.rel = 'stylesheet';
+    fontLink.href = monthFonts[monthClass];
+    document.head.appendChild(fontLink);
 });
