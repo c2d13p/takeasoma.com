00db2ccb21200c755179912f85638115424ebfb2
CDP21 <c2d13p@gmail.com>
2024-09-09 15:59:45 +0200

    add new blog entry

--- js/manageSpin.js
+++ js/manageSpin.js
@@ -9,7 +9,6 @@ let intervalId;
 let isClockwise1 = true;
 let isClockwise2 = false;
 
-// Function to determine speed step based on currentDuration
 function getSpeedStep() {
     if (currentDuration <= 10) return 1;
     else if (currentDuration <= 20) return 2;
@@ -18,31 +17,27 @@ function getSpeedStep() {
     else return 10;
 }
 
-// Function to update animation duration
 function updateAnimationDuration(duration) {
     image1.style.animationDuration = `${duration}s`;
     image2.style.animationDuration = `${duration}s`;
 }
 
-// Function to increase speed (decrease duration)
 function increaseSpeed() {
     let speedStep = getSpeedStep();
-    if (currentDuration > 1) { // Ensure duration doesn't go below minimum
+    if (currentDuration > 1) {
         currentDuration -= speedStep;
         updateAnimationDuration(currentDuration);
     }
 }
 
-// Function to decrease speed (increase duration)
 function decreaseSpeed() {
     let speedStep = getSpeedStep();
-    if (currentDuration < 300) { // Ensure duration doesn't exceed maximum
+    if (currentDuration < 300) {
         currentDuration += speedStep;
         updateAnimationDuration(currentDuration);
     }
 }
 
-// Start increasing or decreasing speed when holding down the button
 function startIncreasingSpeed() {
     clearInterval(intervalId);
     intervalId = setInterval(increaseSpeed, 100);
@@ -52,16 +47,12 @@ function startDecreasingSpeed() {
     intervalId = setInterval(decreaseSpeed, 100);
 }
 
-// Stop changing speed when releasing the button
 function stopChangingSpeed() {
     clearInterval(intervalId);
 }
 
-// Event listeners for the + and - buttons
 increaseSpeedBtn.addEventListener('click', increaseSpeed);
 decreaseSpeedBtn.addEventListener('click', decreaseSpeed);
-
-// For hold functionality
 increaseSpeedBtn.addEventListener('mousedown', startIncreasingSpeed);
 increaseSpeedBtn.addEventListener('mouseup', stopChangingSpeed);
 increaseSpeedBtn.addEventListener('mouseleave', stopChangingSpeed);
@@ -75,19 +66,16 @@ decreaseSpeedBtn.addEventListener('touchstart', startDecreasingSpeed);
 decreaseSpeedBtn.addEventListener('touchend', stopChangingSpeed);
 decreaseSpeedBtn.addEventListener('touchcancel', stopChangingSpeed);
 
-// Function to stop rotation
 function stopRotation() {
     image1.style.animationPlayState = 'paused';
     image2.style.animationPlayState = 'paused';
 }
 
-// Function to resume rotation
 function resumeRotation() {
     image1.style.animationPlayState = 'running';
     image2.style.animationPlayState = 'running';
 }
 
-// Function to invert spinning direction
 function invertSpin() {
     if (isClockwise1) {
         image1.classList.remove('clockwise');
@@ -109,19 +97,15 @@ function invertSpin() {
     isClockwise2 = !isClockwise2;
 }
 
-// Event listeners for desktop (mouse events)
 image1.addEventListener('mousedown', stopRotation);
 image1.addEventListener('mouseup', resumeRotation);
 image2.addEventListener('mousedown', stopRotation);
 image2.addEventListener('mouseup', resumeRotation);
-
-// Event listeners for mobile (touch events)
 image1.addEventListener('touchstart', stopRotation);
 image1.addEventListener('touchend', resumeRotation);
 image2.addEventListener('touchstart', stopRotation);
 image2.addEventListener('touchend', resumeRotation);
 
-// Event listener for the invert spin button
 invertSpinBtn.addEventListener('click', invertSpin);
 
 document.body.style.webkitTouchCallout='none';
@@ -135,11 +119,11 @@ document.addEventListener(
     { passive: false }
   );
 
-  window.oncontextmenu = function(event) {
+document.getElementById('image1').ondragstart = function() { return false; };
+document.getElementById('image2').ondragstart = function() { return false; };
+
+window.oncontextmenu = function(event) {
     event.preventDefault();
     event.stopPropagation();
     return false;
 };
-
-document.getElementById('image1').ondragstart = function() { return false; };
-document.getElementById('image2').ondragstart = function() { return false; };
