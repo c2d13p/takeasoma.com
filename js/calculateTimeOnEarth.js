a919e269467c3357feabfc53cd9ac2afaab0aa42
CDP21 <c2d13p@gmail.com>
2024-09-22 11:24:40 +0200

    remove bug (time display disappears)

--- js/calculateTimeOnEarth.js
+++ js/calculateTimeOnEarth.js
@@ -138,10 +138,8 @@ document.addEventListener('keydown', function(event) {
 
 timeDisplay.addEventListener('mousedown', showQuote);
 timeDisplay.addEventListener('mouseup', restoreTime);
-timeDisplay.addEventListener('mouseleave', restoreTime);
 timeDisplay.addEventListener('touchstart', showQuote);
 timeDisplay.addEventListener('touchend', restoreTime);
-timeDisplay.addEventListener('touchcancel', restoreTime);
 
 let originalTimeText = '';
 
