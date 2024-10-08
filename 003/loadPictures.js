db18c3528f54f861dccb58cf26c63e2270e58dcf
CDP21 <c2d13p@gmail.com>
2024-10-08 22:26:30 +0200

    fix google drive link

--- 003/loadPictures.js
+++ 003/loadPictures.js
@@ -8,7 +8,17 @@ const container = document.getElementById('image-container');
 
 imageIDs.forEach(id => {
     const img = document.createElement('img');
-    img.src = `https://drive.google.com/uc?export=download&id=${id}`;
+    img.src = `https://drive.google.com/thumbnail?id=${id}&sz=s4000`;
     img.className = 'img';
     container.appendChild(img);
+
+    /*
+    sz=s32: 32x32 pixels
+    sz=s64: 64x64 pixels
+    sz=s128: 128x128 pixels
+    sz=s256: 256x256 pixels
+    sz=s512: 512x512 pixels
+    sz=s1024: 1024 pixels in width (height is adjusted to maintain aspect ratio)
+    sz=s4000: 4000 pixels in width (height is adjusted to maintain aspect ratio)
+    */
 });
