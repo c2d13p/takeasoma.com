dcb88a57feb71729d398b01c6e79a8c410bd5987
c2d13p <c2d13p@gmail.com>
2024-10-09 12:45:12 +0200

    fix style

--- 003/loadPictures.js
+++ 003/loadPictures.js
@@ -1,7 +1,8 @@
 const imageIDs = [
-    '16ehE6_8ltOuEA6-ZzRaE6RTgjPsmd7ID',
-    '16ehE6_8ltOuEA6-ZzRaE6RTgjPsmd7ID',
-    '16ehE6_8ltOuEA6-ZzRaE6RTgjPsmd7ID'
+    '1Fm2R_TjqsiG-sNxiWp1onZJzqn0IqYsf',
+    '1k9C633M8PNB6hFZF4ijhj4jD9x2vbXUA',
+    '1lSEIiig3EjoekfNszt3AmlpdoEWDuoj4',
+    '1-E-ggzoQz1lXlzHhtDjhbFseIJZBUcKO'
 ];
 
 const container = document.getElementById('image-container');
@@ -22,3 +23,56 @@ imageIDs.forEach(id => {
     sz=s4000: 4000 pixels in width (height is adjusted to maintain aspect ratio)
     */
 });
+
+window.addEventListener('scroll', function() {
+    const footer = document.getElementById('footer');
+    const header = document.querySelector('.header');
+    const headerLogo = document.getElementById('header-logo');
+    const instagramLogo = document.getElementById('instagram-logo');
+    const scrollPosition = window.scrollY || window.pageYOffset;
+
+    if (scrollPosition > 1) {
+        footer.classList.add('footer-visible');
+        headerLogo.src = "logo-half-1.png";
+        if (window.innerWidth <= 767) {
+            header.style.height = '4vh';
+            instagramLogo.style.height = '60%';
+        } else if (window.innerWidth <= 1023){
+            header.style.height = '6vh';
+            instagramLogo.style.height = '50%';
+        } else {
+            header.style.height = '7.5vh';
+            instagramLogo.style.height = '80%';
+        }
+    } else {
+        footer.classList.remove('footer-visible');
+        headerLogo.src = "logo.png";
+        if (window.innerWidth <= 767) {
+            header.style.height = '8vh';
+            instagramLogo.style.height = '45%';
+        } else if (window.innerWidth <= 1023) {
+            header.style.height = '12vh';
+            instagramLogo.style.height = '45%';
+        } else {
+            header.style.height = '15vh';
+            instagramLogo.style.height = '50%';
+        }
+    }
+});
+
+function updateTimeLeft() {
+    const daysUntilNextMonday = {
+        1: "OUT TODAY",
+        2: "OUT IN 6 DAYS",
+        3: "OUT IN 5 DAYS",
+        4: "OUT IN 4 DAYS",
+        5: "OUT IN 3 DAYS",
+        6: "OUT IN 2 DAYS",
+        0: "OUT IN 1 DAY"
+    };
+
+    const today = new Date().getDay();
+    document.getElementById('time-left').innerText = daysUntilNextMonday[today];
+}
+
+updateTimeLeft();
