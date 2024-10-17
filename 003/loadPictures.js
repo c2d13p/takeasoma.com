4b277f4fbdb33512ca372c7a58c105b1c13fa5f9
CDP21 <c2d13p@gmail.com>
2024-10-17 18:47:23 +0200

    restructure to add ambient effect better

--- 003/loadPictures.js
+++ 003/loadPictures.js
@@ -6,48 +6,6 @@ const imageIDs = [
     { id: '1-E-ggzoQz1lXlzHhtDjhbFseIJZBUcKO', title: '2016-08-11' }
 ];
 
-const container = document.getElementById('image-container');
-
-imageIDs.forEach((image, index) => {
-    /*
-    sz=s32: 32x32 pixels
-    sz=s64: 64x64 pixels
-    sz=s128: 128x128 pixels
-    sz=s256: 256x256 pixels
-    sz=s512: 512x512 pixels
-    sz=s1024: 1024 pixels in width (height is adjusted to maintain aspect ratio)
-    sz=s4000: 4000 pixels in width (height is adjusted to maintain aspect ratio)
-    */
-    const img = document.createElement('img');
-    img.src = `https://drive.google.com/thumbnail?id=${image.id}&sz=s1024`;
-    img.className = 'img';
-    img.setAttribute('data-ambient', '');
-    
-    const title = document.createElement('div');
-    title.textContent = image.title;
-    title.className = 'img-title';
-    
-    const imgWrapper = document.createElement('div');
-    imgWrapper.className = 'img-wrapper';
-    imgWrapper.id = `img-${index + 1}`;
-
-    imgWrapper.appendChild(img);
-    imgWrapper.appendChild(title);
-    
-    container.appendChild(imgWrapper);
-});
-
-window.addEventListener('scroll', function() {
-    const footer = document.getElementById('footer');
-    const scrollPosition = window.scrollY || window.pageYOffset;
-
-    if (scrollPosition > 1) {
-        footer.classList.add('footer-visible');
-    } else {
-        footer.classList.remove('footer-visible');
-    }
-});
-
 function updateTimeLeft() {
     const daysUntilNextMonday = {
         1: "OUT TODAY",
@@ -65,5 +23,49 @@ function updateTimeLeft() {
 
 updateTimeLeft();
 
-var ambient = new Ambient ({blur: 20, retainAttributes: true, includeCSS: false});
-ambient.mount ();
+window.addEventListener('scroll', function() {
+    const footer = document.getElementById('footer');
+    const scrollPosition = window.scrollY || window.pageYOffset;
+
+    if (scrollPosition > 1) {
+        footer.classList.add('footer-visible');
+    } else {
+        footer.classList.remove('footer-visible');
+    }
+});
+
+const container = document.getElementById('container');
+
+imageIDs.forEach((image, index) => {
+    /*
+    sz=s32: 32x32 pixels
+    sz=s64: 64x64 pixels
+    sz=s128: 128x128 pixels
+    sz=s256: 256x256 pixels
+    sz=s512: 512x512 pixels
+    sz=s1024: 1024 pixels in width (height is adjusted to maintain aspect ratio)
+    sz=s4000: 4000 pixels in width (height is adjusted to maintain aspect ratio)
+    */
+    const img = document.createElement('img');
+    img.src = `https://drive.google.com/thumbnail?id=${image.id}&sz=s1024`;
+    img.className = 'img';
+    
+    const imgWrapper = document.createElement('div');
+    imgWrapper.className = 'img-wrapper';
+    imgWrapper.id = `img-${index + 1}`;
+    imgWrapper.style.backgroundImage = `url('https://drive.google.com/thumbnail?id=${image.id}&sz=s1024')`;
+
+    imgWrapper.appendChild(img);
+    
+    const imgTitle = document.createElement('div');
+    imgTitle.textContent = image.title;
+    imgTitle.className = 'img-title';
+
+    const imgContainer = document.createElement('div');
+    imgContainer.className = 'img-container';
+
+    imgContainer.appendChild(imgWrapper);
+    imgContainer.appendChild(imgTitle);
+
+    container.appendChild(imgContainer);
+});
