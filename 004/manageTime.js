281dbd9ebfa026f8d40f5694f65caadce0b7cdea
CDP21 <c2d13p@gmail.com>
2024-11-15 11:29:18 +0100

    add analogue clock

--- 004/manageTime.js
+++ 004/manageTime.js
@@ -1,44 +1,93 @@
-const divider = document.getElementById('divider');
+const modeToggle = document.getElementById("mode-toggle");
 
-function toggleDivider() {
-    if (divider.style.visibility === 'hidden') {
-        divider.style.visibility = 'visible';
+function setCookie(name, value, days) {
+    const d = new Date();
+    d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "Expires=" + d.toUTCString();
+    document.cookie = `${name}=${value}; ${expires}; path=/`;
+}
+  
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i].trim();
+      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
+function toggleMode() {
+    if (modeToggle.textContent === "ANALOGUE") {
+        setCookie("mode", "analogue", 365);
+        loadMode();
+        modeToggle.textContent = "DIGITAL";
     } else {
-        divider.style.visibility = 'hidden';
+        setCookie("mode", "digital", 365);
+        loadMode();
+        modeToggle.textContent = "ANALOGUE";
     }
 }
 
-setInterval(toggleDivider, 1000);
+modeToggle.addEventListener("click", toggleMode);
 
-function toggleDarkMode() {
+function loadMode() {
+    const mode = getCookie('mode') || 'digital';
+
     const now = new Date();
     const hour = now.getHours();
-
     const isNightTime = hour >= 20 || hour < 8;
+    const theme = isNightTime ? "dark" : "light";
 
-    const placeholder = document.getElementById('placeholder');
+    const fontFamily = mode === 'digital' ? "Ticking Timebomb BB" : "Roboto Mono";
+    const fontWeight = mode === 'digital' ? 500 : 700;
+
+    const background = document.getElementById('background');
     const time = document.getElementById('time');
-    const divider = document.getElementById('divider');
+    const second = document.getElementById('second');
 
-    if (isNightTime) {
-        placeholder.src = 'placeholder-dark-mode.png';
-        time.src = 'time-dark-mode.png';
-        divider.src = 'divider-dark-mode.png';
+    background.src = 'img/' + mode + '-background-' + theme + '.png';
+    time.src = 'img/' + mode + '-time-' + theme + '.png';
+    second.src = 'img/' + mode + '-second-' + theme + '.png';
 
-        
-        document.body.style.backgroundColor = '#000000';
-        document.querySelectorAll('.controls a').forEach(a => a.style.color = '#F92C27');
-        document.querySelectorAll('button').forEach(button => button.style.color = '#F92C27');
+    modeToggle.textContent = mode === 'digital' ? "ANALOGUE" : "DIGITAL";
+    document.body.style.fontFamily = fontFamily
+    document.querySelectorAll('button').forEach(button => {
+        button.style.fontFamily = fontFamily;
+        button.style.fontWeight = fontWeight;
+    });
+    document.body.style.fontWeight = fontWeight
+
+    if (theme === 'dark') {
+        document.body.style.backgroundColor = mode === 'digital' ? '#000000': '#333333';
+        document.querySelectorAll('a').forEach(a => a.style.color = mode === 'digital' ? '#F92C27': '#ffffff');
+        document.querySelectorAll('button').forEach(button => button.style.color = mode === 'digital' ? '#F92C27': '#ffffff');
     } else {
-        placeholder.src = 'placeholder-light-mode.png';
-        time.src = 'time-light-mode.png';
-        divider.src = 'divider-light-mode.png';
-
         document.body.style.backgroundColor = '#E7E7E7'; 
-        document.querySelectorAll('.controls a').forEach(a => a.style.color = '#333333');
-        document.querySelectorAll('button').forEach(button => button.style.color = '#333333');
+        document.querySelectorAll('a').forEach(a => a.style.color = '#333333');
+        document.querySelectorAll('button').forEach(button => button.style.color = '#333333');    
     }
 }
 
-window.onload = toggleDarkMode;
-setInterval(toggleDarkMode, 300000); 
+function manageSecond() {
+    const mode = getCookie('mode') || 'digital';
+    const second = document.getElementById('second');
+
+    if (mode === 'digital') {
+        if (second.style.visibility === 'hidden') {
+            second.style.visibility = 'visible';
+        } else {
+            second.style.visibility = 'hidden';
+        }
+    } else {
+        second.style.visibility = 'visible';
+        second.style.transform = 'translate(-50%, -50%) rotate(5deg)';
+        setTimeout(() => {
+            second.style.transform = 'translate(-50%, -50%) rotate(0deg)';
+        }, 100); // Adjust this timing for how long you want the "attempt" to last
+    }
+}
+
+window.onload = loadMode;
+setInterval(loadMode, 300000);
+setInterval(manageSecond, 1000);
