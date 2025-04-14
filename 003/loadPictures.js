7a68ba2807be93c01c08be576c0d60ed412c943b
CDP21 <c2d13p@gmail.com>
2025-04-14 09:38:12 +0200

    publish 003 picture

--- 003/loadPictures.js
+++ 003/loadPictures.js
@@ -1,4 +1,5 @@
 const imageIDs = [
+  { id: "1XvNcKHa6_zdqC6cqjKjDFtUZJNiLTNBV", title: "2025-04-14" },
   { id: "1VoY1KQGpV_U_yKeCrcuaO2ScwhoZC5I2", title: "2025-04-07" },
   { id: "1jKptGBQ3iUqh6sI6lhuQX5nEj-R-nuH6", title: "2025-03-31" },
   { id: "1tuX8g4ts6ovUYJKQNWA2kamtSTN6DD1I", title: "2025-03-24" },
