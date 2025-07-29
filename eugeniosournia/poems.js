de6b7098915b5cc373166cc23d339663584709a4
CDP21 <c2d13p@gmail.com>
2025-07-29 14:18:23 +0200

    add poems

---
+++ eugeniosournia/poems.js
@@ -0,0 +1,16 @@
+/*  setCookie(name, value, days) {
+    const d = new Date();
+    d.setTime(d.getTime() + days * 24 * 60 * 60 * 1000);
+    const expires = "Expires=" + d.toUTCString();
+    document.cookie = `${name}=${value}; ${expires}; path=/`;
+  }
+
+  getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(";");
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i].trim();
+      if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }*/
