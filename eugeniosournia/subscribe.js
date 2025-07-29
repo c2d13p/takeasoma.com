0df906dcc5d6d9fddab046f71f2a040ec1d7f75d
CDP21 <c2d13p@gmail.com>
2025-07-29 15:12:41 +0200

    add subscription functionality

---
+++ eugeniosournia/subscribe.js
@@ -0,0 +1,22 @@
+document
+  .getElementById("subscribe-button")
+  .addEventListener("click", subscribe);
+
+function subscribe() {
+  var email = document.getElementById("email").value;
+  var button = document.getElementById("subscribe-button");
+
+  if (email.trim() === "") {
+    alert("Please enter an email");
+    return;
+  }
+
+  console.log(`New subscription ${email}`);
+
+  // Fill the hidden form input and submit it
+  document.getElementById("hidden-entry").value = email;
+  document.getElementById("hidden-form").submit();
+
+  button.textContent = "Subscribed!";
+  button.disabled = true;
+}
